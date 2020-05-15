d279412352a75387d102ab0d1aa14308cb1c2159
StasBelousov <stasbelousov@icloud.com>
2020-05-15 17:38:44 +0300

    Initial Commit

---
+++ SunnyDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunnyDay
+//
+//  Created by Станислав Белоусов on 15/05/2020.
+//  Copyright © 2020 Станислав Белоусов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
